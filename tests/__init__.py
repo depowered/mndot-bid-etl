11f587b99fc6ca5049a68ee8d5d35eeb62d66113
depowered <dpwr16@gmail.com>
2022-09-08 13:23:37 -0500

    add test_items data setup

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,10 @@
 from pathlib import Path
 
-from mndot_bid_etl.transform.abstract import Abstract, read_abstract_csv
+from mndot_bid_etl.transform.abstract import read_abstract_csv
+from mndot_bid_etl.transform.item_list import read_item_list_csv
 
 # Setup test abstract data
-csv_file = Path("./tests/data/test_abstract.csv").resolve()
-test_abstract = read_abstract_csv(csv_file)
+test_abstract = read_abstract_csv(Path("./tests/data/test_abstract.csv").resolve())
+
+# Setup test items data
+test_items = read_item_list_csv(Path("./tests/data/test_items.csv").resolve())
