445d8c203b42a0a5ccb400d432fd8ea54c9edf24
depowered <dpwr16@gmail.com>
2022-09-12 14:17:47 -0500

    remove unused code

--- mndot_bid_etl/transform/recipes/bid.py
+++ mndot_bid_etl/transform/recipes/bid.py
@@ -32,6 +32,7 @@ def rename_bidder_ids(name: str) -> str:
     return name.strip().split(" ")[0].lower()
 
 
+# ---------- Define Transformation Schema ----------
 bid_transformation_schema = TransformationSchema(
     column_schemas=[
         ColumnSchema(
@@ -68,43 +69,20 @@ bid_transformation_schema = TransformationSchema(
 )
 
 
-# ---------- Define Transformations ----------
+# ---------- Create Transformations ----------
 filter_columns = FilterColumns(
-    fuzzy_filter_list=bid_transformation_schema.get_filter_columns_list(
+    bid_transformation_schema.get_filter_columns_list(
         use_source_column_search_string=True
     )
-    # fuzzy_filter_list=[
-    #     "ContractId",
-    #     "ItemNumber",
-    #     "ItemDescription",
-    #     "Quantity",
-    #     "(Unit Price)",
-    # ]
 )
 
 modify_values = ModifyValues(
-    fuzzy_modify_map=bid_transformation_schema.get_modify_values_map(
+    bid_transformation_schema.get_modify_values_map(
         use_source_column_search_string=True
     )
-    # fuzzy_modify_map={
-    #     "ItemNumber": format_item_id,
-    #     "ItemDescription": format_long_description,
-    #     "Quantity": format_quantity,
-    #     "(Unit Price)": format_price,
-    # }
 )
 
-rename_columns = RenaneColumns(
-    fuzzy_rename_map=bid_transformation_schema.get_rename_columns_map()
-    # fuzzy_rename_map={
-    #     "ContractId": "contract_id",
-    #     "ItemNumber": "item_id",
-    #     "ItemDescription": "long_description",
-    #     "Quantity": "quantity",
-    #     "(Unit Price)": lambda x: x.strip().split(" ")[0].lower(),
-    # }
-)
-
+rename_columns = RenaneColumns(bid_transformation_schema.get_rename_columns_map())
 
 melt = Melt(
     id_vars=["contract_id", "item_id", "long_description", "quantity"],
@@ -112,18 +90,7 @@ melt = Melt(
     value_name="unit_price",
 )
 
-
-cast_columns = CastColumns(
-    fuzzy_dtype_map=bid_transformation_schema.get_cast_columns_map()
-    # fuzzy_dtype_map={
-    #     "contract_id": DType.STRING,
-    #     "item_id": DType.STRING,
-    #     "long_description": DType.STRING,
-    #     "quantity": DType.FLOAT64,
-    #     "bidder_id": DType.STRING,
-    #     "unit_price": DType.INT64,
-    # }
-)
+cast_columns = CastColumns(bid_transformation_schema.get_cast_columns_map())
 
 
 # ---------- Construct Transformer ----------
