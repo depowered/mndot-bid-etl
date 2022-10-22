86ec8c8e56fbfa42a439f8b261ceceea1a50f74c
depowered <dpwr16@gmail.com>
2022-10-22 17:54:55 +0000

    remove specific year arguments from create_spec_year_matrix

--- mndot_bid_etl/transform/spec_year.py
+++ mndot_bid_etl/transform/spec_year.py
@@ -2,6 +2,11 @@ import pandas as pd
 
 
 def _generate_in_spec_year_df(df: pd.DataFrame) -> pd.DataFrame:
+    """Generates a Dataframe with the following index structure and a spec year column filled with True.
+
+    index: `{spec_code}_{unit_code}_{item_code}_{long_description}`
+    index example: `2011_601_00003_CONSTRUCTION SURVEYING`
+    """
     spec_year = df.at[0, "spec_year"]
 
     merged_id = (
@@ -18,16 +23,12 @@ def _generate_in_spec_year_df(df: pd.DataFrame) -> pd.DataFrame:
     return out_df.set_index("merged_id")
 
 
-def transform_spec_year_matrix(
-    df_2016: pd.DataFrame, df_2018: pd.DataFrame, df_2020: pd.DataFrame
-) -> pd.DataFrame:
+def create_spec_year_matrix(tranformed_item_dfs: list[pd.DataFrame]) -> pd.DataFrame:
 
-    in_2016 = _generate_in_spec_year_df(df_2016)
-    in_2018 = _generate_in_spec_year_df(df_2018)
-    in_2020 = _generate_in_spec_year_df(df_2020)
+    spec_year_dfs = [_generate_in_spec_year_df(df) for df in tranformed_item_dfs]
 
-    spec_year_matrix = in_2016.join(other=[in_2018, in_2020], how="outer").fillna(
-        value=False
+    spec_year_matrix = (
+        spec_year_dfs[0].join(other=spec_year_dfs[1:], how="outer").fillna(value=False)
     )
 
     return spec_year_matrix
